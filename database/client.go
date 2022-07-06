f07d652f5471352b3e87c3f0dee382d5e27e0ef1
Scipiia <56618443+Scipiia@users.noreply.github.com>
2022-07-06 21:15:24 +0800

    Update client.go

--- database/client.go
+++ database/client.go
@@ -9,36 +9,6 @@ import (
 	"time"
 )
 
-//func DatabaseInstance() *mongo.Client {
-//mongoDB := "mongodb://localhost:27017"
-//fmt.Print(mongoDB)
-//client, err := mongo.NewClient(options.Client().ApplyURI(mongoDB))
-//if err != nil {
-//	log.Fatalln(err)
-//}
-//ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-//defer cancel()
-//
-//err = client.Connect(ctx)
-//if err != nil {
-//	log.Fatal(err)
-//}
-//
-//fmt.Println("\nconnected to mongodb")
-//
-//return client
-//}
-
-//var Client *mongo.Client = DatabaseInstance()
-
-// database open collection
-//func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-//	var collection *mongo.Collection = client.Database("mongodb1").Collection("posts")
-//	return collection
-//}
-
-// *******************************************************//
-
 func DatabaseInstance() *mongo.Client {
 	mongoDB := "mongodb://localhost:27017"
 
